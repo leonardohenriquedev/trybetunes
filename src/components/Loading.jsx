21ceb22d656c7eecdc5f65f0d042ef724fc55d2d
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-12-11 00:05:58 -0300

    Envia projeto

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Loading extends Component {
+  render() {
+    return <div className="loading"><p>Carregando... 🎶</p></div>;
+  }
+}
