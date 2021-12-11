f2d990281e46cc157bf863db6c5e8ef775d1a66c
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-12-11 00:12:37 -0300

    att

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Loading extends Component {
-  render() {
-    return <div className="loading"><p>Carregando... 🎶</p></div>;
-  }
-}
