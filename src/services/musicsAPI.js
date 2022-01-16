67c67d87c8e27eac15097ca43608ea64c22a6300
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2022-01-16 19:18:48 -0300

    att

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,7 +1,7 @@
 // import fetch from 'node-fetch';
 
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {mode: 'cors'});
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {mode: "cors"});
   const requestJson = await request.json();
   return requestJson.results;
 };
